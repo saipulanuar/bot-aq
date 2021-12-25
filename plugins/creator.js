54e08a624f6e32aa5a718c31e0a9779856c9e819
saipulanuar <77055141+saipulanuar@users.noreply.github.com>
2021-12-25 17:45:39 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,8 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
   // F this.sendContact(m.chat, '62815158600891', 'Nurutomo', m)
-  this.sendContact(m.chat, '6288279268363''6288279268363', 'King Of Bear', m)
+  this.sendContact(m.chat, '6288279268363', 'King Of Bear', m)
+  this.sendContact(m.chat, '6288279268363', 'King Of Bear', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
