5cc21f03061ad635b9b262e92cae0ed4df65f36a
saipulanuar <77055141+saipulanuar@users.noreply.github.com>
2021-12-28 21:05:06 +0700

    Update tebakgambar.js

--- plugins/tebakgambar.js
+++ plugins/tebakgambar.js
@@ -19,7 +19,7 @@ Ketik ${usedPrefix}hint untuk hint
 Bonus: ${poin} XP
     `.trim()
   conn.tebakgambar[id] = [
-    await conn.sendFile(m.chat, json.result.images, 'tebakgambar.jpg', caption, m, false, { thumbnail: Buffer.alloc(0) }),
+    await conn.sendFile(m.chat, json.result.url, 'tebakgambar.jpg', caption, m, false, { thumbnail: Buffer.alloc(0) }),
     json, poin,
     setTimeout(() => {
       if (conn.tebakgambar[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.result.jawaban}*`, conn.tebakgambar[id][0])
