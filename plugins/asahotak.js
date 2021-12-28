e5948bcd83e8f1cd0fc16fc8a1f7a4116db0cc22
saipulanuar <77055141+saipulanuar@users.noreply.github.com>
2021-12-28 13:28:39 +0700

    Update asahotak.js

--- plugins/asahotak.js
+++ plugins/asahotak.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 
 let timeout = 120000
-let poin = 500
+let poin = 1000
 let handler = async (m, { conn, usedPrefix }) => {
     conn.asahotak = conn.asahotak ? conn.asahotak : {}
     let id = m.chat
